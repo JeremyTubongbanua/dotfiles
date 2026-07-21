3494ef3ff5b71b4c9e3f26f75fe5c670dc66cad0
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-07-21 13:01:41 -0400

    feat: lualine 2 lines long

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,19 +4,35 @@ return {
     'nvim-tree/nvim-web-devicons'
   },
   config = function()
+    vim.o.laststatus = 0  -- no per-window bottom statusline; tabline + winbar cover it
+
     require('lualine').setup({
-      winbar = {
+      options = {
+        globalstatus = true,
+      },
+      tabline = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      winbar = {
         lualine_c = {
           {
             'filename',
             path = 1  -- 0 = just filename, 1 = relative path, 2 = absolute path, 3 = absolute path with ~ for home
           },
         },
-        lualine_x = {'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+      },
+      inactive_winbar = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1
+          },
+        },
       },
       sections = {},
     })
