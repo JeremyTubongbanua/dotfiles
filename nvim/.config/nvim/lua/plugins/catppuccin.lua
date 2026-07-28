343a3b844e264d80e69d71412990c03ee898222c
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-07-28 12:33:32 -0400

    feat: go back to tokyonight

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -3,22 +3,7 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    require('catppuccin').setup({
-      flavour = "mocha",
-      custom_highlights = function(colors)
-        return {
-          -- Example custom overrides for fugitive/diff blocks
-          fugitiveHash = { fg = colors.yellow },
-          fugitiveHeader = { fg = colors.mauve, style = { "bold" } },
-          fugitiveStagedHeading = { fg = colors.green, style = { "bold" } },
-          fugitiveUnstagedHeading = { fg = colors.red, style = { "bold" } },
-
-          -- Standard diff views inside fugitive status/commit buffers
-          diffAdded = { fg = colors.green },
-          diffRemoved = { fg = colors.red },
-        }
-      end,
-    })
-    vim.cmd.colorscheme('catppuccin')
+    require('catppuccin').setup({})
+    vim.cmd.colorscheme('catppuccin-mocha')
   end,
 }
