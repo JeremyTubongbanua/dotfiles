29d66eff32da730a2af5a78d1dc966fe1e2feccd
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-01 18:28:06 -0400

    refactor(nvim): markdown-preview.lua

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,10 @@
 return {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  cmd = {
+    "MarkdownPreviewToggle",
+    "MarkdownPreview",
+    "MarkdownPreviewStop" 
+  },
   build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
