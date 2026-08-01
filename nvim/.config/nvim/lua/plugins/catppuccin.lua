8c03856eeb2be5fac243d7bfcc1f15c66979b97c
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-01 13:41:02 -0400

    feat(nvim): catppuccin theme

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,13 @@
+-- theme
 return {
   "catppuccin/nvim",
   name = "catppuccin",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000,
   config = function()
-    -- require('catppuccin').setup({})
-    -- vim.cmd.colorscheme('catppuccin-mocha')
+    require('catppuccin').setup({
+      flavour = "mocha",
+    })
+    vim.cmd.colorscheme('catppuccin-mocha')
   end,
 }
