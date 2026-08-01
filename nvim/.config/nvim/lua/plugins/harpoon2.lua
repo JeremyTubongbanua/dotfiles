c42f9f1a8ad3e197b1bfec710f572806822460b7
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-01 18:55:35 -0400

    feat(nvim): remove unused stuff in harpoon

--- nvim/.config/nvim/lua/plugins/harpoon2.lua
+++ nvim/.config/nvim/lua/plugins/harpoon2.lua
@@ -3,11 +3,9 @@ return {
     branch = "harpoon2",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      -- "nvim-telescope/telescope.nvim",
     },
     config = function()
       local harpoon = require('harpoon');
-
       harpoon:setup({
         settings = {
           save_on_toggle = true,
@@ -19,23 +17,6 @@ return {
           end,
         },
       })
-      -- Configure UI window size
-      -- harpoon:extend({
-      --   UI_CREATE = function(cx)
-      --     vim.keymap.set("n", "<C-v>", function()
-      --       harpoon.ui:select_menu_item({ vsplit = true })
-      --     end, { buffer = cx.bufnr })
-      --
-      --     vim.keymap.set("n", "<C-x>", function()
-      --       harpoon.ui:select_menu_item({ split = true })
-      --     end, { buffer = cx.bufnr })
-      --
-      --     vim.keymap.set("n", "<C-t>", function()
-      --       harpoon.ui:select_menu_item({ tabedit = true })
-      --     end, { buffer = cx.bufnr })
-      --   end,
-      -- })
-
       vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Add file to Harpoon' })
       vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Toggle Harpoon Quick Menu' })
     end,
