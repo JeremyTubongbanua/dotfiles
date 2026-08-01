00a47beafa1096f88ffe07c09ec23398f67bfcc6
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-01 18:54:47 -0400

    chore: remove commands from keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,29 +1,18 @@
 -- vim.o, vim.g are all meta accessors
+
+-- Leader keys
 vim.g.mapleader = " " -- leader key is space
 vim.g.maplocalleader = "\\"
 
--- Custom keymaps for Neovim (non-plugin related)
-vim.api.nvim_create_user_command("Q", "qall", { desc = "Quit all windows" })
-vim.api.nvim_create_user_command("T", "terminal", { desc = "Open terminal in current window" })
-
--- delete all buffers except current
-vim.api.nvim_create_user_command("BDA", function()
-  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
-  local current_buf = vim.api.nvim_get_current_buf()
-  for _, buf in ipairs(bufs) do
-    if buf.bufnr ~= current_buf then
-      vim.api.nvim_buf_delete(buf.bufnr, { force = false })
-    end
-  end
-end, { desc = "Delete all buffers except current" });
-
--- Movement keymaps
+-- Move a line (or collection of lines)
 vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
 vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
 vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
 vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
 
--- vim.keymap.set('n', '0', '0^', { desc = "Beginning of line" })
+-- Go to beginning of line (first character, even if indented)
+vim.keymap.set('n', '0', '0^', { desc = "Beginning of line" })
 
+-- Indent a selection without having to re-select
 vim.keymap.set('v', '>', '>gv', { desc = "Indent right and reselect" })
 vim.keymap.set('v', '<', '<gv', { desc = "Indent left and reselect" })
