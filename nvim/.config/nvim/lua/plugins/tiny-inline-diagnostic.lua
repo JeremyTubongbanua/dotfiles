97e10036f96866eb096835d7bae7e5155f7c99b3
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 13:51:13 -0400

    chore(nvim): just comemnts in tiny-inline-diagnostic.lua

--- nvim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -1,6 +1,7 @@
 return {
   'rachartier/tiny-inline-diagnostic.nvim',
-  event = 'LspAttach',
+  event = "VeryLazy",
+  priority = 1000,
   opts = {
     preset = 'modern',
     options = {
@@ -14,3 +15,13 @@ return {
     },
   },
 }
+
+-- return {
+--   "rachartier/tiny-inline-diagnostic.nvim",
+--   event= "VeryLazy",
+--   priority = 1000,
+--   config = function()
+--     require("tiny-inline-diagnostic").setup()
+--     vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+--   end,
+-- }
