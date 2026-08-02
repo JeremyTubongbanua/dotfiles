d6ae213577974bcc4634aa386f097b6cddd5316e
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 18:24:18 -0400

    feat(nvim): flash hopping back and fort

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -3,6 +3,14 @@
 return {
   'folke/flash.nvim',
   event = "VeryLazy",
+  opts = {
+    modes = {
+      char = { -- `f'` then keep pressing `f` to hop between matches (`F` to go back)
+        enabled = true,
+        jump_labels = true,
+      },
+    },
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
