c05131f4ce69f8972fbac52ebdfeab116a5a7f87
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 18:01:25 -0400

    feat(nvim): disable mini.completion when using snacks picker

--- nvim/.config/nvim/lua/plugins/mini.completion.lua
+++ nvim/.config/nvim/lua/plugins/mini.completion.lua
@@ -4,4 +4,13 @@ return {
   version = '*',
   opts = {},
   enabled = true,
+  init = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'snacks_picker_input',
+      callback = function()
+        vim.b.minicompletion_disable = true
+      end,
+      desc = 'Disable mini.completion in snacks picker input',
+    })
+  end,
 }
