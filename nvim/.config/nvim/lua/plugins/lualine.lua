40340f528603197945b0d1bce42cf8fade6e3d68
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 13:48:25 -0400

    feat(nvim): use mini.icons for everything now

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    'echasnovski/mini.diff',
   },
   config = function()
     require('lualine').setup({
--- nvim/.config/nvim/lua/plugins/mini.icons.lua
+++ nvim/.config/nvim/lua/plugins/mini.icons.lua
@@ -1,4 +1,13 @@
+-- icon provider -- https://github.com/nvim-mini/mini.icons
+-- nvim-web-devicons is not installed; nvim-tree and oil both ask for it by
+-- name, so mock it here and they get mini.icons instead.
 return {
   'nvim-mini/mini.icons',
-  version = false
+  version = false,
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('mini.icons').setup()
+    MiniIcons.mock_nvim_web_devicons()
+  end,
 }
