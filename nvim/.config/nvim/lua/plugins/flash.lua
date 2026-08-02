48ca9496680d9f8dd07d66d36592982708cbb515
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 15:09:45 -0400

    chore(nvim): clean up flash.lua

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -2,11 +2,7 @@
 ---@type LazyPluginSpec
 return {
   'folke/flash.nvim',
-  config = function()
-    require('flash').setup({})
-  end,
   event = "VeryLazy",
-  opts = {},
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
