50d2afaa4a37e1714cf2cc10c4550158b5274f79
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 14:13:07 -0400

    feat(nvim): separate colorscheme from catppuccin.lua

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,44 +1,41 @@
--- theme
 ---@type LazyPluginSpec
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   lazy = false,
   priority = 1000,
-  config = function()
-    require('catppuccin').setup({
-      flavour = "auto",
-      background = {
-        light = "latte", dark = "mocha"
-      },
-      integrations = {
-        native_lsp = {
-          underlines = {
-            errors = {
-              "undercurl"
-            }
-          },
+  opts = {
+    flavour = "auto",
+    background = {
+      light = "latte",
+      dark = "mocha"
+    },
+    integrations = {
+      native_lsp = {
+        underlines = {
+          errors = {
+            "undercurl"
+          }
         },
       },
-      custom_highlights = function(colors)
-        return {
-          DiagnosticUnderlineError = {
-            fg = colors.red,
-            sp = colors.red,
-            undercurl = true,
-          },
-          diffAdded = {
-            link = "DiffAdd"
-          },
-          diffRemoved = {
-            link = "DiffDelete"
-          },
-          diffChanged = {
-            link = "DiffChange"
-          },
-        }
-      end,
-    })
-    vim.cmd.colorscheme('catppuccin-nvim')
-  end,
+    },
+    custom_highlights = function(colors)
+      return {
+        DiagnosticUnderlineError = {
+          fg = colors.red,
+          sp = colors.red,
+          undercurl = true,
+        },
+        diffAdded = {
+          link = "DiffAdd"
+        },
+        diffRemoved = {
+          link = "DiffDelete"
+        },
+        diffChanged = {
+          link = "DiffChange"
+        },
+      }
+    end,
+  },
 }
