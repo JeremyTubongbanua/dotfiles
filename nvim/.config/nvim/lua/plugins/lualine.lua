820355273bc1af9745ddbc25d4a6d198b0c8b30e
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-02 18:01:43 -0400

    feat(nvim): oil buffer lines and opts = {} format for lualine.lua

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,49 +1,50 @@
+-- oil buffers have no real file name, so show the directory oil is browsing instead
+local oil_dir = {
+  function()
+    local dir = require('oil').get_current_dir(0)
+    return dir and vim.fn.fnamemodify(dir, ':~') or ''
+  end,
+  cond = function()
+    return vim.bo.filetype == 'oil'
+  end,
+}
+
+local filename = {
+  'filename',
+  -- 0 == just file name
+  -- 1 == relative path
+  -- 2 == absolute path
+  -- 3 == abosulte path with ~ for home
+  path = 1,
+  cond = function()
+    return vim.bo.filetype ~= 'oil' and vim.fn.expand('%:t') ~= ''
+  end,
+}
+
 ---@type LazyPluginSpec
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
     'echasnovski/mini.diff',
   },
-  config = function()
-    require('lualine').setup({
-      options = {
-        globalstatus = true,
-      },
-      tabline = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {},
-        lualine_x = {'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-      winbar = {
-        lualine_c = {
-          {
-            'filename',
-            -- 0 == just file name
-            -- 1 == relative path
-            -- 2 == absolute path
-            -- 3 == abosulte path with ~ for home
-            path = 1,
-            cond = function()
-              return vim.fn.expand('%:t') ~= ''
-            end,
-          },
-        },
-      },
-      inactive_winbar = {
-        lualine_c = {
-          {
-            'filename',
-            path = 1,
-            cond = function()
-              return vim.fn.expand('%:t') ~= ''
-            end,
-          },
-        },
-      },
-      sections = {},
-    })
-  end,
+  opts = {
+    options = {
+      globalstatus = true,
+    },
+    tabline = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_c = {},
+      lualine_x = {'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+    },
+    winbar = {
+      lualine_c = { oil_dir, filename },
+    },
+    inactive_winbar = {
+      lualine_c = { oil_dir, filename },
+    },
+    sections = {},
+  },
 }
