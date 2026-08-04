6ae3191edf92f26138d3af041076e8701bf083ac
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-04 16:20:57 -0400

    fix(nvim): fix the annoying dart ls LSP fugitive warnings

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd("BufReadPre", {
+  desc = "Keep LSP clients off fugitive:// blobs",
+  group = vim.api.nvim_create_augroup("no-lsp-on-fugitive", { clear = true }),
+  pattern = "fugitive://*",
+  callback = function(event)
+    vim.bo[event.buf].buftype = "nowrite"
+  end,
+})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Strip trailing whitespace on save",
   group = vim.api.nvim_create_augroup("strip-trailing-whitespace", { clear = true }),
