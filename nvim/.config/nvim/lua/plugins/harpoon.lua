a7d44e478420333d7dd22e2588aa0af8b95caa06
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-08-13 13:14:12 -0400

    chore(format): harpoon.lua

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,24 +1,24 @@
 ---@type LazyPluginSpec
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local harpoon = require('harpoon');
-      harpoon:setup({
-        settings = {
-          save_on_toggle = true,
-          sync_on_ui_close = true,
-        },
-        default = {
-          get_root_dir = function()
-            return vim.loop.cwd()
-          end,
-        },
-      })
-      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Add file to Harpoon' })
-      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Toggle Harpoon Quick Menu' })
-    end,
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local harpoon = require('harpoon');
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+      },
+      default = {
+        get_root_dir = function()
+          return vim.loop.cwd()
+        end,
+      },
+    })
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Add file to Harpoon' })
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Toggle Harpoon Quick Menu' })
+  end,
 }
